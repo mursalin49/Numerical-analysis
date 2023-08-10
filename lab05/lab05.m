d7d0c32ce73e8e18a21f62f2e45a08514965b19d
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-08-10 18:05:50 +0600

    Add files via upload

---
+++ lab05/lab05.m
@@ -0,0 +1,38 @@
+y = @(x) (x^3)-(23*x^2)+(142*x)-120;
+
+guess1 = input("Enter first guess: " );
+guess2 = input("Enter second guess: " );
+max_iterations = input("Enter max iterations: " );
+tolerance = input("Enter tolerance: " );
+
+iterations = 0;
+errors = [];
+prevGuess = 0;
+
+while iterations < max_iterations
+    iterations = iterations + 1;
+
+    newGuess = (guess1 + guess2) / 2;
+    if y(newGuess) == 0
+        root = newGuess;
+        break;
+    elseif y(newGuess) * y(guess1) < 0
+        guess2 = newGuess;
+    else
+        guess1 = newGuess;
+    endif
+
+    if iterations > 1
+        relative_error = abs(newGuess - prevGuess) / newGuess;
+        errors = [errors, relative_error];
+        if relative_error < tolerance
+          root = newGuess;
+          break;
+        endif
+    endif
+
+    prevGuess=newGuess;
+endwhile
+fprintf("No of iterations: %d \n",iterations);
+fprintf("Root: %d \n",root);
+plot([1:1:length(errors)],errors);
